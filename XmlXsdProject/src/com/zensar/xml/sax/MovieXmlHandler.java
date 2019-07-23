c7d8ef2188b7b2de39171a90fc608ce480dff905
Manali Sharma <manalis318@gmail.com>
2019-07-22 18:08:45 -0700

    modified class file MovieXmlHandler

--- XmlXsdProject/src/com/zensar/xml/sax/MovieXmlHandler.java
+++ XmlXsdProject/src/com/zensar/xml/sax/MovieXmlHandler.java
@@ -9,25 +9,25 @@ public class MovieXmlHandler extends DefaultHandler {
 	@Override
 	public void startDocument() throws SAXException {
 		// TODO Auto-generated method stub
-		super.startDocument();
+		System.out.println("Start of document...");
 	}
 
 	@Override
 	public void endDocument() throws SAXException {
 		// TODO Auto-generated method stub
-		super.endDocument();
+		System.out.println("End of document");
 	}
 
 	@Override
 	public void startElement(String uri, String localName, String qName, Attributes attributes) throws SAXException {
 		// TODO Auto-generated method stub
-		super.startElement(uri, localName, qName, attributes);
+		System.out.println("Start Element: "+qName);
 	}
 
 	@Override
 	public void endElement(String uri, String localName, String qName) throws SAXException {
 		// TODO Auto-generated method stub
-		super.endElement(uri, localName, qName);
+		System.out.println("End Element: "+qName);
 	}
 
 	@Override
